78c99c5194b5d6b1e096e6fe1a9d519b82dcdad4
Agmetrix <124325944+anointing1234@users.noreply.github.com>
2025-09-05 08:26:57 +0100

    Update settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bank_app.middleware.SessionTimeoutMiddleware', 
 ]
 
 ROOT_URLCONF = 'bank.urls'
