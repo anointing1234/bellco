342cf8938e337fd3d2619e64cbddec554a134fc8
anointing1234 <yakubudestiny9@gmail.com>
2025-09-06 09:16:00 +0100

    commted

--- bank/settings.py
+++ bank/settings.py
@@ -44,7 +44,7 @@ SESSION_COOKIE_SECURE = True  # Use secure cookies over HTTPS
 CSRF_COOKIE_SECURE = True    # Use secure CSRF cookies over HTTPS
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Expire session when browser closes
 SESSION_COOKIE_AGE = 0
-SECURE_SSL_REDIRECT = True  # Redirect HTTP to HTTPS       # Ensure no expiration time is set for session cookies  # Ensures sessions end when browser closes
+     # Ensure no expiration time is set for session cookies  # Ensures sessions end when browser closes
 
 # Application definition
 AUTH_USER_MODEL = 'bank_app.Account'
