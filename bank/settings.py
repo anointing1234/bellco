88c9eb29ffc72c270bd95fe490a638f291b0bad2
Agmetrix <124325944+anointing1234@users.noreply.github.com>
2026-01-04 07:12:54 +0100

    Comment out ALLOWED_HOSTS in settings.py
    Comment out the ALLOWED_HOSTS setting for security.

--- bank/settings.py
+++ bank/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-@^^&_3r$p1tr#jxmb#nx3q*oi#wo%bxr!x^n(a%61pu@=#jq%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ["*"]
 #CSRF_TRUSTED_ORIGINS = ["https://belcoccu.com"]
 #ALLOWED_HOSTS = ["belcoccu.com"]
 
